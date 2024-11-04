e5c84ddc0d268cdd05426f09cef54117017b7f64
Maksym Bondarevskyi <bondarevskiymaks@gmail.com>
2024-11-04 20:31:09 +0200

    several changes

--- cinema/serializers.py
+++ cinema/serializers.py
@@ -15,4 +15,5 @@ class MovieSerializer(serializers.Serializer):
         instance.title = validated_data.get("title", instance.title)
         instance.description = validated_data.get("description", instance.description)
         instance.duration = validated_data.get("duration", instance.duration)
+        instance.save()
         return instance
